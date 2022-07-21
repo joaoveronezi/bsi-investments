ad1ee35fe1a3ccf880538e389300cdc8ed8eb670
Joao Veronezi <jvveronezi@corra.com>
2022-07-20 22:15:13 -0300

    Adding some links to the website

--- src/components/WhatsappButton/WhatsappButton.jsx
+++ src/components/WhatsappButton/WhatsappButton.jsx
@@ -2,15 +2,9 @@ import React from "react";
 import { whatsIcon } from "../../assets/icons/v2";
 import { WhatsappButtonWrapper } from "./WhatsappButton.styles";
 
-const WhatsappButton = () => {
-  console.log("Teste");
-
-  // PEDIR O LINK DO WPP DA GALERA DA BSI
-  return (
-    <WhatsappButtonWrapper>
-      <img src={whatsIcon} alt="Whatsapp icon" />
-    </WhatsappButtonWrapper>
-  );
-};
-
+const WhatsappButton = () => (
+  <WhatsappButtonWrapper>
+    <img src={whatsIcon} alt="Whatsapp icon" />
+  </WhatsappButtonWrapper>
+);
 export default WhatsappButton;
