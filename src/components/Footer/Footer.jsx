3176182a61c5c88b04d14d7b6b7ffd419063588d
Thiago_S_C <112531304+23-ThiagoSartori@users.noreply.github.com>
2024-11-29 16:34:22 -0300

    Update Footer.jsx
    Alteração de texto footer - troca de nome blacksheep >> BSI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ import {
 } from "./Footer.style";
 
 const Footer = () => {
-  const text = "A Black Sheep Assessor de Investimentos Ltda é uma empresa de assessoria de investimento devidamente registrada na Comissão de Valores Mobiliários (CVM), na formada Resolução CVM nº 178/2023. A Black Sheep Assessor de Investimentos Ltda atua no mercado financeiro credenciada à XP Investimentos CCTVM S/A,o que pode ser verificado através do site da CVM, site da ANCORD  ou através do site da própria XP Investimentos CCTVM S/A (www.xpi.com.br > Encontre um escritório > Selecione abaixo o estado e a cidade que deseja pesquisar > Veja a lista dos agentes autônomos). Na forma da legislação da CVM, o Assessor de Investimento não pode administrar ou gerir o patrimônio de investidores. O Assessor de investimentos é um intermediário e depende da autorização prévia do cliente para realizar operações no mercado financeiro. O investimento em ações é um investimento de risco e rentabilidade passada não é garantia de rentabilidade futura. Na realização de operações com derivativos existe a possibilidade de perdas superiores aos valores investidos, podendo resultar em significativas perdas patrimoniais. Para informações e dúvidas, favor contatar seu Assessor de Investimentos. Para reclamações, favor contatar a Ouvidoria da XP Investimentos no telefone nº0800-722-3730.";
+  const text = "A BSI Assessor de Investimentos Ltda é uma empresa de assessoria de investimento devidamente registrada na Comissão de Valores Mobiliários (CVM), na formada Resolução CVM nº 178/2023. A BSI Assessor de Investimentos Ltda atua no mercado financeiro credenciada à XP Investimentos CCTVM S/A,o que pode ser verificado através do site da CVM, site da ANCORD  ou através do site da própria XP Investimentos CCTVM S/A (www.xpi.com.br > Encontre um escritório > Selecione abaixo o estado e a cidade que deseja pesquisar > Veja a lista dos agentes autônomos). Na forma da legislação da CVM, o Assessor de Investimento não pode administrar ou gerir o patrimônio de investidores. O Assessor de investimentos é um intermediário e depende da autorização prévia do cliente para realizar operações no mercado financeiro. O investimento em ações é um investimento de risco e rentabilidade passada não é garantia de rentabilidade futura. Na realização de operações com derivativos existe a possibilidade de perdas superiores aos valores investidos, podendo resultar em significativas perdas patrimoniais. Para informações e dúvidas, favor contatar seu Assessor de Investimentos. Para reclamações, favor contatar a Ouvidoria da XP Investimentos no telefone nº0800-722-3730.";
   return (
     <FooterWrapper>
       <IconAndAdressWrapper>
@@ -81,7 +81,7 @@ const Footer = () => {
       <DisclaimerWrapper>
         <p>
           <strong>
-            © Black Sheep Investimentos uma empresa credenciada da
+            © BSI uma empresa credenciada da
             XP investimentos.
           </strong>
           Todos os direitos reservados.
