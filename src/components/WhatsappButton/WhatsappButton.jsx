67c1b1b5bcb8633aa31200061dacb15543765975
Thiago_S_C <112531304+23-ThiagoSartori@users.noreply.github.com>
2024-11-29 16:42:08 -0300

    Update WhatsappButton.jsx
    Alteração de Button do whatsapp

--- src/components/WhatsappButton/WhatsappButton.jsx
+++ src/components/WhatsappButton/WhatsappButton.jsx
@@ -4,7 +4,7 @@ import { WhatsappButtonWrapper } from "./WhatsappButton.styles";
 
 const WhatsappButton = () => (
   <a
-    href="https://wa.me/message/WESFBCHTQ6HYC1"
+    href="https://wa.me/553131915591"
     target="_blank"
     rel="noreferrer"
   >
