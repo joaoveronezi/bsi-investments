31a7e3604a5de70a408c034c8d886b5e87624575
jveronezi <jveronezi@fullstacklabs.co>
2025-01-14 19:35:28 -0300

    fix: build adjustments 2

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -20,21 +20,21 @@ const Accordion = () => (
           <AccordionHeader>CONHEÇA NOSSO TIME DE ASSESSORES</AccordionHeader>
         </AccordionComponent.Header>
         <AccordionComponent.Body>
-  <h5>Arthur Neves Trindade</h5>
-  <a href="https://www.linkedin.com/in/arthur-trindade-b73261144/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-  <h5>Fabio Muller dos Reis de Salles Pupo</h5>
-  <a href="https://www.linkedin.com/in/fabio-pupo-63608b6a/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-  <h5>Fernando Hitoshi Watanabe</h5>
-  <a href="https://www.linkedin.com/in/fernando-hitoshi-watanabe/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-  <h5>João Paulo Pereira Silva</h5>
-  <a href="https://www.linkedin.com/in/jo%C3%A3o-paulo-pereira-silva-825396194/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-  <h5>Lucas Torres</h5>
-  <a href="https://www.linkedin.com/in/torresxp/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-  <h5>Ricardo Souza Ribeiro</h5>
-  <a href="https://www.linkedin.com/in/ricardo-ribeiro-385587282/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-  <h5>Thiago Sartori Cardoso</h5>
-  <a href="https://www.linkedin.com/in/ricardo-ribeiro-385587282/" style={{ fontWeight: 'bold' }}>CONTATO</a>
-</AccordionComponent.Body>
+          <h5>Arthur Neves Trindade</h5>
+          <a href="https://www.linkedin.com/in/arthur-trindade-b73261144/" style={{ fontWeight: "bold" }}>CONTATO</a>
+          <h5>Fabio Muller dos Reis de Salles Pupo</h5>
+          <a href="https://www.linkedin.com/in/fabio-pupo-63608b6a/" style={{ fontWeight: "bold" }}>CONTATO</a>
+          <h5>Fernando Hitoshi Watanabe</h5>
+          <a href="https://www.linkedin.com/in/fernando-hitoshi-watanabe/" style={{ fontWeight: "bold" }}>CONTATO</a>
+          <h5>João Paulo Pereira Silva</h5>
+          <a href="https://www.linkedin.com/in/jo%C3%A3o-paulo-pereira-silva-825396194/" style={{ fontWeight: "bold" }}>CONTATO</a>
+          <h5>Lucas Torres</h5>
+          <a href="https://www.linkedin.com/in/torresxp/" style={{ fontWeight: "bold" }}>CONTATO</a>
+          <h5>Ricardo Souza Ribeiro</h5>
+          <a href="https://www.linkedin.com/in/ricardo-ribeiro-385587282/" style={{ fontWeight: "bold" }}>CONTATO</a>
+          <h5>Thiago Sartori Cardoso</h5>
+          <a href="https://www.linkedin.com/in/ricardo-ribeiro-385587282/" style={{ fontWeight: "bold" }}>CONTATO</a>
+        </AccordionComponent.Body>
       </AccordionComponent.Item>
     </AccordionComponent>
   </AccordionWrapper>
